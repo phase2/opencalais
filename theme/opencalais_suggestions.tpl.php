b9565bfb31b8f6d69de9cdac978a2bf427c9da37
Frank Febbraro <frank@phase2technology.com>
2010-12-20 18:44:03 -0500

    added some styling, fixed permissions, and added configuration around putting tags in vertical tabs.

--- theme/opencalais_suggestions.tpl.php
+++ theme/opencalais_suggestions.tpl.php
@@ -4,6 +4,7 @@
  */   
 $score_value = array(0 => t('Low'), 1 => t('Low'), 2 => t('Moderate'), 3 => t('High'));
 $id = $field_name . '_suggestions';
+$for = 'edit-' . strtr(drupal_strtolower($field_name), array(' ' => '-', '_' => '-', '[' => '-', ']' => ''));
 ?>
 <div id='<?php print $id; ?>' class='suggestions'>
   <?php print t('Tag Suggestions: '); ?>
@@ -15,7 +16,7 @@ $id = $field_name . '_suggestions';
       $class = "score-{$size}";
       $term_name = check_plain($term);
       ?>
-    <label class='suggestion <?php print $class; ?>' for='edit-taxonomy-tags-$vid' title='<?php print $hover; ?>'><?php print $term_name; ?></label>
+    <label class='suggestion <?php print $class; ?>' for='<?php print $for; ?>' title='<?php print $hover; ?>'><?php print $term_name; ?></label>
   <?php endforeach; ?>
 <?php endif; ?>
 </div>
