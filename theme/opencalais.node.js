68837a3796d727036adc097ca7e7d096bbcdc60d
Frank Febbraro <frank@phase2technology.com>
2010-12-20 16:07:15 -0500

    renamed calais_api to opencalais_api and all associated variable renamed. Added first pass at ajax inline tagging

---
+++ theme/opencalais.node.js
@@ -0,0 +1,11 @@
+/**
+ * @file opencalais.node.js
+ *    This file contains all javascript associated with the Node edit screens.
+ */
+
+
+Drupal.behaviors.opencalaisNodeEdit = {
+  attach: function(context) {
+    // Do some stuff to attach the suggestions to the taxonomy fields
+  }
+};
---
+++ theme/opencalais_suggestions.tpl.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ *
+ */   
+$score_value = array(0 => t('Low'), 1 => t('Low'), 2 => t('Moderate'), 3 => t('High'));
+$id = $field_name . '_suggestions';
+?>
+<div id='<?php print $id; ?>' class='suggestions'>
+  <?php print t('Tag Suggestions: '); ?>
+<?php
+  if (!empty($suggestions)):
+    foreach ($suggestions as $term => $relevance):
+      $size = ceil(3 * $relevance);
+      $hover = t('Relevance: ') . $score_value[$size];
+      $class = "score-{$size}";
+      $term_name = check_plain($term);
+      ?>
+    <label class='suggestion <?php print $class; ?>' for='edit-taxonomy-tags-$vid' title='<?php print $hover; ?>'><?php print $term_name; ?></label>
+  <?php endforeach; ?>
+<?php endif; ?>
+</div>
