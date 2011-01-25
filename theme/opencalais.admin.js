829c487fa260ec5aee4d7825bdb52573bbfe7f49
= <=>
2011-01-25 14:43:51 -0500

    Changed some form layout stuff and added an ajax handler to help prevent dupe presets for a content type

--- theme/opencalais.admin.js
+++ theme/opencalais.admin.js
@@ -7,26 +7,31 @@
 Drupal.behaviors.opencalaisEntityConfig = {
   attach: function (context) {
     $(".threshold_slider", context).each(function() {
-      $(this).parent().addClass('slider_holder');
-      $(this).hide();
- 
-      var threshold = $(this);
-      var container = $(this).parent("div");     
-      var name = $(this).attr('name');
-      var slider_name = name + '-slider';
-      var slider = $('<div>').attr('id', slider_name).insertBefore($(this));
-      var label_id = slider_name + '-label';
-      var label = $('<div>').html(threshold.val()).attr({id: label_id, class: 'slider_label'}).insertAfter(slider);      
-      slider.slider({
-        min: 0.00,
-  			max: 1.00,
-  			step: 0.01,
-  			value: threshold.val(),
-        slide: function(event, ui ) {
-          threshold.val(ui.value);
-          label.html(ui.value);
-        },
-      });
+      if(!this.processed){
+        
+        $(this).parent().addClass('slider_holder');
+        $(this).hide();
+   
+        var threshold = $(this);
+        var container = $(this).parent("div");     
+        var name = $(this).attr('name');
+        var slider_name = name + '-slider';
+        var slider = $('<div>').attr('id', slider_name).insertBefore($(this));
+        var label_id = slider_name + '-label';
+        var label = $('<div>').html(threshold.val()).attr({id: label_id, class: 'slider_label'}).insertAfter(slider);      
+        slider.slider({
+          min: 0.00,
+    			max: 1.00,
+    			step: 0.01,
+    			value: threshold.val(),
+          slide: function(event, ui ) {
+            threshold.val(ui.value);
+            label.html(ui.value);
+          },
+        });
+        
+        this.processed = true;
+      }
     });
   }
 };
